a1b19a875c2147483b04a57868ff61ba22cfbaa7
Masetto96 <stefano.scola@protonmail.com>
2024-03-19 15:19:11 +0100

    added gridsearch for tuning hyperparam

--- models_gym.py
+++ models_gym.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 
 class Classifier:
@@ -8,23 +10,29 @@ class Classifier:
         self.clf = RandomForestClassifier()
         # define the hyperparameter search space
         self.param_grid = {
+            "bootstrap": [True, False],
+            "max_depth": [3, 5, 10, None],
             "n_estimators": [50, 75, 100],
             "max_features": ["sqrt", "log2", None],
             "criterion": ["gini", "entropy"],
-            "min_samples_leaf": [1, 2, 4],
+            "max_features": [1, 3, 5, 7],
+            "min_samples_leaf": [1, 2, 3],
+            "min_samples_split": [1, 2, 3],
         }
 
-    def train_and_optimize(self, X, y):
-        # fit the model and optimize it on f1 score by randomly selecting the configuration of the paramters
-        random_search = RandomizedSearchCV(
-            estimator=self.clf,
-            param_distributions=self.param_grid,
-            n_iter=25,
-            cv=10,
-            scoring="f1_macro",
-            n_jobs=-1,
-            random_state=42,
-        )
-        random_search.fit(X, y)
-        # return the best model
-        return random_search.best_estimator_
+    def train_and_optimize(self, X, y, n_iter=25, scoring="f1_macro"):
+        # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
+        hyper_search_model = GridSearchCV(
+            param_grid=self.param_grid, estimator=self.clf, cv=10, verbose=3
+        ).fit(X, y)
+
+        print("Best hyperparameters are: " + str(hyper_search_model.best_params_))
+        print("Best score is: " + str(hyper_search_model.best_score_))
+        self.clf = hyper_search_model.best_estimator_
+
+    def evaluate_best_model(self, X_test, y_test):
+
+        print("Score: ", self.clf.score(X_test, y_test))
+        print("Report: ", classification_report(y_test, self.clf.predict(X_test), output_dict=True))
+        ConfusionMatrixDisplay.from_estimator(self.clf, X_test, y_test)
+        plt.show()
--- train_best.py
+++ train_best.py
@@ -1,11 +1,8 @@
+import numpy as np
 from data_loader import DataLoader
 from model_factory import FeatureSelector
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.metrics import classification_report
 from models_gym import Classifier
 
-import numpy as np
-# Number of features to select
 TRAIN_DATA_PATH = "archive/TrainSet.csv"
 TEST_DATA_PATH = "archive/TestSet.csv"
 
@@ -18,18 +15,17 @@ if __name__ == "__main__":
     X_test, y_test = data_loader.prepare_test_data()
 
     # Select train and test features
-    X_sel, selector = fs.select_statistically(X_train, y_train)
+    X_train_reduced, selector = fs.select_statistically(X_train, y_train)
     mask = selector.get_support()
     x_reduced_indices = np.where(mask)[0].astype(int)
     X_test_reduced = X_test[:, x_reduced_indices]
 
-    cl = Classifier()
-    best_cl = cl.train_and_optimize(X_train, y_train)
+    clf = Classifier()
 
-    cl_report = classification_report(y_test, best_cl.predict(X_test_reduced) output_dict=True)
+    clf.train_and_optimize(X_train_reduced, y_train)
 
-    print("Score: ", best_cl.score(X_test_reduced, y_test))
-    print(classification_report)
+    clf.evaluate_best_model(X_test_reduced, y_test)
+
 
     
         
